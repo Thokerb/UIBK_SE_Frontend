ee44a3843cb92f465a0960a579f560f021d9c92e
David Manuel Meyer <david.meyer@student.uibk.ac.at>
2021-04-16 19:56:55 +0200

    Dashboard now uses PrimeNG Button correctly

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {ButtonModule} from "primeng/button";
 
 
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        ButtonModule
+    ]
 })
 export class TodoModule { }
