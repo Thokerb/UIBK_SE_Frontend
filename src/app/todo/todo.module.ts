7b94b2b1a94c0b34ad2b9cf75bd833aae4115e83
David Manuel Meyer <david.meyer@student.uibk.ac.at>
2021-04-16 20:42:48 +0200

    Added ripple effect to dashboard buttons

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {ButtonModule} from "primeng/button";
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from 'primeng/ripple';
 
 
 
 @NgModule({
   declarations: [DashboardComponent],
-    imports: [
-        CommonModule,
-        ButtonModule
-    ]
+  imports: [
+    CommonModule,
+    ButtonModule,
+    RippleModule
+  ]
 })
 export class TodoModule { }
