71be622e75d475b4cc0a3471e0c5c19928c1c8d2
David Manuel Meyer <david.meyer@student.uibk.ac.at>
2021-05-11 22:55:31 +0200

    created user profile page component stub

---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserProfilePageComponent } from './user-profile-page/dashboard.component';
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from 'primeng/ripple';
+import {FormsModule} from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [UserProfilePageComponent],
+    imports: [
+        CommonModule,
+        ButtonModule,
+        RippleModule,
+        FormsModule
+    ]
+})
+export class ProfileModule { }
---
+++ src/app/profile/user-profile-page/user-profile-page.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {TodoSelector} from '../../redux/todo/todo.selector';
+import {TodoReducer} from '../../redux/todo/todo.reducer';
+import {TodoAction} from '../../redux/todo/todo.action';
+import {RestServiceService} from '../../api/rest-service.service';
+import {SocketService} from '../../api/socket.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class UserProfilePageComponent implements OnInit {
+
+  todo: Observable<string[]>;
+  username: string;
+  roomID: string;
+  constructor(private store: Store,
+              private todoSelector: TodoSelector,
+              private todoActions: TodoAction,
+              private restService: RestServiceService,
+              private webSocket: SocketService,
+              private router: Router
+              ) { }
+
+  ngOnInit(): void {
+    this.todo = this.store.select(this.todoSelector.selectAllTodos);
+  }
+
+  onLobbyBtn(): void {
+    this.router.navigateByUrl('/gamelobby');
+  }
+
+  onCreateGameBtn(): void {
+    this.router.navigateByUrl('/createGame');
+  }
+
+  onPlayerProfileBtn(): void {
+    this.router.navigateByUrl('/profile');
+  }
+
+  openCubeSite(): void {
+    this.router.navigateByUrl('/cube');
+  }
+  sendToSocket(): void {
+    this.webSocket.send();
+  }
+}
