2db6a3f16636c4c72cc06f375da36f829e4fa074
Thomas Anton Kerber <Thomas.Kerber@student.uibk.ac.at>
2021-05-30 13:25:13 +0200

    lifecycle of selector

--- src/app/login-section/login-form/login-form.component.ts
+++ src/app/login-section/login-form/login-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AuthenticationSelector} from '../../redux/authentication/authentication.selector';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {Router} from '@angular/router';
 import {AuthenticationAction} from '../../redux/authentication/authentication.action';
 import {MessageService} from 'primeng/api';
@@ -16,21 +16,24 @@ export class LoginFormComponent implements OnInit {
   isAuthenticated: Observable<boolean>;
   username: string;
   password: string;
+  last: boolean;
+  private loginErrorSubscription: Subscription;
   constructor(private store: Store, private selector: AuthenticationSelector, private router: Router, private authAction: AuthenticationAction,
               private messageService: MessageService) {
     this.isAuthenticated = this.store.select(this.selector.selectAuthStatus);
-    this.store.select(this.selector.selectLoginErrorStatus).subscribe(next => {
-      if (next){
-        console.log(next);
+    this.loginErrorSubscription = this.store.select(this.selector.selectLoginErrorStatus).subscribe(next => {
+      if (next && this.last !== next){
         this.messageService.add({severity: 'error', summary: 'Login', detail: `Falscher Benutzername oder Passwort!`});
         this.store.dispatch(authAction.setLoginError({status: false}));
       }
       else{
 
       }
+      this.last = next;
     });
     this.isAuthenticated.subscribe(value => {
       if (value){
+        this.loginErrorSubscription.unsubscribe();
         router.navigateByUrl('/dashboard');
       }
       else{
