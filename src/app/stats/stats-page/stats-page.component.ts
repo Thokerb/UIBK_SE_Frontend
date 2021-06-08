944c269088ea4e2e127d23c7805b4d63999530e1
David Manuel Meyer <david.meyer@student.uibk.ac.at>
2021-06-08 18:39:49 +0200

    Refactoring (removed unused code)

--- src/app/stats/stats-page/stats-page.component.ts
+++ src/app/stats/stats-page/stats-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
-import {TodoSelector} from '../../redux/todo/todo.selector';
-import {TodoReducer} from '../../redux/todo/todo.reducer';
-import {TodoAction} from '../../redux/todo/todo.action';
 import {RestServiceService} from '../../api/rest-service.service';
 import {SocketService} from '../../api/socket.service';
 import {Router} from '@angular/router';
