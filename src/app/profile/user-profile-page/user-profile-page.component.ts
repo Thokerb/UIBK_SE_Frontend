c56c076cce9df476d23d0c8ddf2d887fa33c6f28
David Manuel Meyer <david.meyer@student.uibk.ac.at>
2021-06-08 17:05:15 +0200

    User roles are now capitalized

--- src/app/profile/user-profile-page/user-profile-page.component.ts
+++ src/app/profile/user-profile-page/user-profile-page.component.ts
@@ -8,7 +8,7 @@ import {RestServiceService} from '../../api/rest-service.service';
 import {SocketService} from '../../api/socket.service';
 import {Router} from '@angular/router';
 import {AuthenticationSelector} from '../../redux/authentication/authentication.selector';
-import {User} from '../../api/dto/UserManagement';
+import {User, USER_ROLES} from '../../api/dto/UserManagement';
 
 @Component({
   selector: 'app-user-profile',
@@ -18,6 +18,7 @@ import {User} from '../../api/dto/UserManagement';
 export class UserProfilePageComponent implements OnInit {
 
   user: Partial<User> | null;
+  userRolesText: string | null;
   constructor(private store: Store,
               private restService: RestServiceService,
               private webSocket: SocketService,
@@ -25,12 +26,27 @@ export class UserProfilePageComponent implements OnInit {
               private authSelector: AuthenticationSelector
               ) {
     this.user = null;
+    this.userRolesText = null;
+  }
+
+  /**
+   * Transforms array of userRoles into capitalized string
+   */
+  userRolesToText(userRoles: USER_ROLES[] | undefined): string {
+    if ((userRoles === undefined) || (userRoles.length < 1)) {
+      return null;
+    }
+    return userRoles
+      .map((userRole) => userRole.toString())
+      .map((userRole: string) => userRole[0].toUpperCase() + userRole.slice(1).toLowerCase())
+      .join(', ');
   }
 
   ngOnInit(): void {
 
     this.store.select(this.authSelector.selectCurrentUser).subscribe(userResult => {
       this.user = userResult;
+      this.userRolesText = this.userRolesToText(this.user.roles);
       console.log(this.user.email);
     });
   }
